7a79c506fcff274a830f1cf4aeca18531e1a3dc0
AngadKevin <70760372+AngadKevin@users.noreply.github.com>
2020-12-21 13:37:05 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,27 @@
+onEvent("buttonmessages", "click", function( ) {
+  open("https://ifaketextmessage.com/");
+});
+onEvent("buttoncalendar", "click", function( ) {
+  open("https://www.google.com/calendar/about/");
+});
+onEvent("buttoncamera", "click", function( ) {
+  open("https://www.microsoft.com/store/productId/9WZDNCRFJBBG");
+});
+onEvent("buttonweather", "click", function( ) {
+  open("https://www.microsoft.com/store/productId/9WZDNCRFJ3Q2");
+});
+onEvent("buttontube", "click", function( ) {
+  open("https://www.youtube.com/?gl=IN");
+});
+onEvent("button1", "click", function( ) {
+  open("https://skribbl.io/");
+});
+onEvent("button2", "click", function( ) {
+  open("https://anikabasu2.whjr.site/");
+});
+onEvent("button3", "click", function( ) {
+  open("https://code.whitehatjr.com/s/dashboard");
+});
+onEvent("button4", "click", function( ) {
+  open("https://community.whitehatjr.com/");
+});
